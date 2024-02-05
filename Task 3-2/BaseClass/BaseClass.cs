d01a00d54327f7267a3b1171194937cabd326867
Dasha <dariastreltsova148@gmail.com>
2024-02-05 20:14:27 +0300

    heredity

---
+++ Task 3-2/BaseClass/BaseClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace BaseClass
+{
+    public abstract class BaseClass{
+        protected abstract void UserInput();
+        protected abstract void RandomInput();
+        public abstract void Output();
+        public abstract void average();
+
+    }
+}
